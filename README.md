8df08c375af7a372236b101d63a4573616df6986
Light <lochois@gmail.com>
2023-10-12 21:45:50 +1100

    update template with chi and templ support

--- README.md
+++ README.md
@@ -5,9 +5,10 @@
 This template uses a unique combination of pure htmx for interactivity plus Go templates for creating components and forms, also included:
 - Built-in added styling to show off some of Tailwind and Daisyui.
 - Uses HTMX for MPA style interactivity on a single page as per SPA.
-- Added custom middleware for handling HTMX calls.
-- Built-in version display linked to version variable from main which can be updated on app build and during runtime via a function.
+- Added custom Chi middleware for handling HTMX calls in an easy to maintain routing configuration.
+- Built-in version display linked to version variable from main which can be updated on app build for CICD and/or during runtime.
 - Scripts configured to use the Bun runtime to launch Vite. (Make sure you have bun installed first)
+- Also using https://templ.guide/ for components and templates use "templ generate" to update templ files. 
 
 
 ## Live Development
--- app.tmpl.go
+++ app.tmpl.go
@@ -1,8 +1,13 @@
 package main
 
 import (
+	components "changeme/templates"
+	"changeme/types"
 	"context"
-	"fmt"
+
+	"github.com/a-h/templ"
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
 )
 
 // App struct
@@ -38,7 +43,13 @@ func (a *App) shutdown(ctx context.Context) {
 	// Perform your teardown here
 }
 
-// Greet returns a greeting for the given name
-func (a *App) Greet(name string) string {
-	return fmt.Sprintf("Hello %s, It's show time!", name)
+func NewChiRouter() *chi.Mux {
+	r := chi.NewRouter()
+	r.Use(middleware.Logger)
+	r.Use(middleware.Recoverer)
+	r.Get("/initial", templ.Handler(components.Pages(types.IndexForm{Pages: []types.Page{{Label: "Greet Form", Path: "/greet"}},
+		Version: types.AppVersion{Version: "0.0.1", UpdateText: "No update available"}})).ServeHTTP)
+	r.Get("/greet", templ.Handler(components.GreetForm("/greet", "#result", "Submit")).ServeHTTP)
+	r.Post("/greet", components.Greet)
+	return r
 }
